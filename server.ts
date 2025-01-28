171b2f64f56ff9148384eed3c91beacf366c4ee7
Jay Kapoor <jkap86@gmail.com>
2025-01-28 15:26:20 -0500

    add self pinging for heroku

--- server.ts
+++ server.ts
@@ -1,11 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import https from "https";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  const interval = 25 * 60 * 1000;
+  const url = "https://adp-background-de9ea0eff9b9.herokuapp.com/";
+
+  setInterval(() => {
+    https
+      .get(url, (res) => {
+        console.log(`Pinged ${url}. Status: ${res.statusCode}`);
+      })
+      .on("error", (err) => {
+        console.error(`Error pinging ${url}: ${err.message}`);
+      });
+  }, interval);
+}
 
 const PORT = process.env.PORT || 5000;
 
